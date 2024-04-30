a16df1e653cef3185751e22f0e02bd612b1d9353
euivmarUPV <euivmar@upvnet.upv.es>
2024-04-30 14:49:31 +0200

    Update installHomeassistant.sh

--- installHomeassistant.sh
+++ installHomeassistant.sh
@@ -2,16 +2,14 @@
 
 # Actualizar el sistema
 echo "Updating the system..."
-sudo apt update && sudo apt upgrade -y
+sudo apt-get update && sudo apt-get upgrade -y
 
 # Instalación de Docker y Docker-compose
 echo "Installing Docker and Docker-compose..."
-sudo apt install curl -y
-sudo curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo rm get-docker.sh
-sudo apt install -y libffi-dev libssl-dev python3-dev python3 python3-pip
-sudo apt install docker-compose -y
+sudo apt-get install curl -y
+sudo curl -fsSL https://get.docker.com | sudo bash
+sudo apt-get install -y libffi-dev libssl-dev python3-dev python3 python3-pip
+sudo apt-get install docker-compose -y
 sudo systemctl enable docker
 user=$(whoami)
 sudo usermod -aG docker $user
@@ -26,7 +24,7 @@ sudo su -
 
 # Instalar dependencias
 echo "Installing necessary dependencies..."
-apt install apparmor jq wget curl udisks2 libglib2.0-bin network-manager dbus systemd-journal-remote cifs-utils lsb-release nfs-common systemd-resolved -y
+apt-get install apparmor jq wget curl udisks2 libglib2.0-bin network-manager dbus systemd-journal-remote cifs-utils lsb-release nfs-common systemd-resolved -y
 
 # Descargar e instalar os-agent
 echo "Downloading and installing os-agent..."
@@ -40,7 +38,7 @@ gdbus introspect --system --dest io.hass.os --object-path /io/hass/os || echo "E
 # Descargar e instalar Home Assistant Supervised
 echo "Downloading and installing Home Assistant Supervised..."
 wget -O homeassistant-supervised.deb https://github.com/home-assistant/supervised-installer/releases/latest/download/homeassistant-supervised.deb
-apt install ./homeassistant-supervised.deb
+apt-get install ./homeassistant-supervised.deb
 
 # Reiniciar el sistema
 echo "Restarting the system..."
